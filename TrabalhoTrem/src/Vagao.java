693bfc1a8862e147d70a03285a340b0fb6107743
Pedro Haggstram <pedrofamh@protonmail.com>
2023-08-23 13:58:32 +0000

    Criada as classes

---
+++ TrabalhoTrem/src/Vagao.java
@@ -0,0 +1,32 @@
+public class Vagao {
+    private int id;
+    private double capacidadeCarga;
+    private Trem trem;
+
+    public Vagao(int id, double capacidadeCarga) {
+        this.id = id;
+        this.capacidadeCarga = capacidadeCarga;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getCapacidadeCarga() {
+        return capacidadeCarga;
+    }
+
+    public Trem getTrem() {
+        return trem;
+    }
+
+    public void setTrem(Trem trem) {
+        this.trem = trem;
+    }
+    @Override
+    public String toString(){
+        return "Vagão: " + id + "," + capacidadeCarga;
+    }
+
+    
+}

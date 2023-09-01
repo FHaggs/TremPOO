7bb97b85de1aab2d1b8aaea9efcedd63274aa48d
Pedro Fam Haggstram <pedrofamh@protonmail.com>
2023-09-01 11:11:19 -0300

    trem

--- TrabalhoTrem/src/Trem.java
+++ TrabalhoTrem/src/Trem.java
@@ -3,11 +3,18 @@ import java.util.ArrayList;
 public class Trem {
     private int id;
 
+    private int maxVagoes;
+    private int maxCapacidade;
+    private int pesoAtual;
+
     private ArrayList<Locomotiva> locomotivas = new ArrayList<Locomotiva>();
     private ArrayList<Vagao> vagoes = new ArrayList<Vagao>();
 
     public Trem(int id){
         this.id = id;
+        maxVagoes = 0;
+        maxCapacidade = 0;
+        pesoAtual = 0;
     }
 
     public boolean addLocomotiva(Locomotiva locomotiva){
@@ -16,15 +23,52 @@ public class Trem {
         }
         locomotivas.add(locomotiva);
         locomotiva.setTrem(this);
+
+        maxVagoes += locomotiva.getMaxVagoes();
+        maxCapacidade += locomotiva.getCapacidadeToneladas();
+
+        if(locomotivas.size() > 2){
+            maxVagoes = maxVagoes - (int)(maxVagoes * 0.1);
+        }
+
+
         return true;
     }
     public boolean addVagao(Vagao vagao){
-        if(locomotivas.isEmpty()){
+        if(locomotivas.isEmpty() || vagoes.size() >= maxVagoes || pesoAtual + vagao.getCapacidadeCarga() > maxCapacidade){
             return false;
         }
         vagoes.add(vagao);
         vagao.setTrem(this);
+        pesoAtual += vagao.getCapacidadeCarga();
         return true;
+    }
+    public Vagao retiraVagao(){
+        if(vagoes.isEmpty()){
+            return null;
+        }
+        Vagao aux = vagoes.get(vagoes.size() - 1);
+
+        pesoAtual -= aux.getCapacidadeCarga();
+
+        vagoes.remove(vagoes.size() - 1);
+        return aux;
+    }
+    public Locomotiva retiraLocomotiva(){
+        if(!vagoes.isEmpty() || locomotivas.isEmpty()){
+            return null;
+        }
+
+        Locomotiva aux = locomotivas.get(locomotivas.size() - 1);
+
+        if(locomotivas.size() > 1){
+            maxVagoes = maxVagoes + (int)(maxVagoes * 0.1);
+        }
+        locomotivas.remove(vagoes.size() - 1);
+        maxCapacidade -= aux.getCapacidadeToneladas();
+        maxVagoes -= aux.getMaxVagoes();
+
+        return aux;
     }
 
 }
